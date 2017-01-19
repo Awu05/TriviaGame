06f27f408ea32c6b05b4c617d71c440d6ea53237
Rael Kenny <RaelKenny@gmail.com>
2017-01-19 16:54:09 -0500

    KaushikVC

---
+++ TriviaGame/KaushikViewController.h
@@ -0,0 +1,17 @@
+//
+//  KaushikViewController.h
+//  TriviaGame
+//
+//  Created by Rael Kenny on 1/19/17.
+//  Copyright © 2017 TrivGame. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KaushikViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *timerLabel;
+@property int counterSecond;
+
+
+@end
