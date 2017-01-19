e5c1d3c1ed0fd2a24101a89f2bcce5ed4acc0254
Andy Wu <andywu@Andys-Mac.local>
2017-01-19 13:09:40 -0500

    Initial Commit

---
+++ TriviaGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TriviaGame
+//
+//  Created by Andy Wu on 1/19/17.
+//  Copyright © 2017 TrivGame. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
