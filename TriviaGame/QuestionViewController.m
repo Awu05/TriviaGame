dde28b550875b6c480bdf1c7ec02d97b1a5b1734
Jerry Chen <ambig22@gmail.com>
2017-01-19 20:04:25 -0500

    updated ui

--- TriviaGame/QuestionViewController.m
+++ TriviaGame/QuestionViewController.m
@@ -17,6 +17,27 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    // Custom Back Button in Nav
+    UIImage *buttonImage = [UIImage imageNamed:@"back.png"];
+    // Change rendering mode to allow tint color
+    buttonImage = [buttonImage imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+    
+    UIButton *backBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    [backBtn setImage:buttonImage forState:UIControlStateNormal];
+    [backBtn setTintColor:[UIColor whiteColor]];
+    
+    backBtn.frame = CGRectMake(0, 0, 25, 12);
+    [backBtn.imageView setContentMode:UIViewContentModeScaleAspectFit];
+    [backBtn addTarget:self action:@selector(back) forControlEvents:UIControlEventTouchUpInside];
+    
+    UIBarButtonItem *customBarItem = [[UIBarButtonItem alloc] initWithCustomView:backBtn];
+    self.navigationItem.leftBarButtonItem = customBarItem;
+    
+}
+
+- (void)back {
+    [self.navigationController popViewControllerAnimated:YES];
 }
 
 @end
